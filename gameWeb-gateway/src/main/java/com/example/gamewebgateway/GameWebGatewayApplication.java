6c178db6e3b14b97dba0394af8e08275f3205871
JunCai <1547598719@qq.com>
2022-10-30 16:48:34 +0800

    第二次测试

---
+++ gameWeb-gateway/src/main/java/com/example/gamewebgateway/GameWebGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamewebgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameWebGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameWebGatewayApplication.class, args);
+    }
+
+}
--- gameWeb-pms/src/test/java/com/example/gamewebpms/GameWebPmsApplicationTests.java
+++ gameWeb-pms/src/test/java/com/example/gamewebpms/GameWebPmsApplicationTests.java
@@ -1,5 +1,11 @@
 package com.example.gamewebpms;
 
+import com.example.gamewebpms.controller.PmsCategoryBrandRelationController;
+import com.example.gamewebpms.controller.PmsCategoryController;
+import com.example.gamewebpms.dao.PmsCategoryDao;
+import com.example.gamewebpms.entity.PmsCategoryEntity;
+import com.example.gamewebpms.service.PmsCategoryService;
+import com.example.gamewebpms.service.impl.PmsCategoryServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +15,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GameWebPmsApplicationTests {
 
+    PmsCategoryService categoryController = new PmsCategoryServiceImpl();
+
     @Test
     public void contextLoads() {
+        PmsCategoryEntity categoryEntity = new PmsCategoryEntity();
+        categoryEntity.setCatId(1L);
+        categoryEntity.setCatLevel(1);
+        categoryEntity.setIcon("a");
+        categoryEntity.setName("JUncai");
+        categoryEntity.setParentCid(1L);
+        categoryEntity.setProductCount(100);
+        categoryEntity.setProductUnit("a");
+        categoryEntity.setShowStatus(0);
+        categoryEntity.setSort(1);
+
+        categoryController.save(categoryEntity);
     }
 
 }
